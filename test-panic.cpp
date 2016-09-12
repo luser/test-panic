a425056ad69548de8861f8805e193e4ba5a6a6c5
Ted Mielczarek <ted@mielczarek.org>
2016-09-12 15:49:08 -0400

    try AddVectoredExceptionHandler

--- test-panic.cpp
+++ test-panic.cpp
@@ -1,6 +1,20 @@
 extern "C" void intentional_panic(const char* message);
 
+#ifdef _WIN32
+#include <Windows.h>
+
+static LONG WINAPI
+fault_handler(LPEXCEPTION_POINTERS exception)
+{
+  return EXCEPTION_CONTINUE_SEARCH;
+}
+
+#endif
+
 int main(int argc, char** argv) {
+#ifdef _WIN32
+  AddVectoredExceptionHandler(1, fault_handler);
+#endif
   intentional_panic(argc > 1 ? argv[1] : "yikes");
   return 0;
 }
